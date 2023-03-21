73ed6e39368084053c8bd32b5c99e19373a57528
YamsDev <105313153+YamsDev@users.noreply.github.com>
2023-03-21 11:07:19 +0000

    Create build.php

---
+++ admin/build.php
@@ -0,0 +1,53 @@
+<?php
+
+    //get battle type
+    $type = $_POST['type'];
+    
+    //get teams
+    $team[0] = $_POST['team_1'];
+    $team[1] = $_POST['team_2'];
+    
+    $arena = $_POST['arena'];
+    
+    //round clock
+    $continue = true;
+    do {
+        //speed
+        //super check
+        //bonus calcs
+        //check for rerolls
+        //if speed tied, highest aqua
+        //if still tied, reroll
+        
+        //each round is 4 attacks
+        for($i = 0; $i < 4; $i++){
+            //attacks
+        //each horse must get at least 1 attack
+        
+        //roll for attack (fire)
+        //check super
+        //bonuses
+        
+        //dodge (dark)
+        //if successful (dubs), damage is 1
+        //if super, deal dmg to attacker
+        //bonuses
+        
+        //defence (earth)
+        //roll for defence
+        //check super (attacker takes dmg if super)
+        //bonuses
+        //defender reroll if applicable
+        //attacker reroll if applicable
+        //compute damage, min of 1
+        
+        //apply damage
+        }
+
+        
+    }
+    while ($continue == true);
+    
+    //attack clock
+
+?>
