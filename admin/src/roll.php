063b83f1f61de0c2d9cba396b1e3aacd757e686d
YamsDev <105313153+YamsDev@users.noreply.github.com>
2023-03-26 14:41:55 +0100

    Update roll.php
    Added checks for supers and rerolls

--- admin/src/roll.php
+++ admin/src/roll.php
@@ -7,5 +7,28 @@
         
         return $result;
     }
+    
+    function superCheck($rolls){
+        $num = $rolls.length;
+        for($i = 1; $i < $num; $i ++){
+            if ($rolls[0] == $rolls[$i]){
+                $super = true;
+            } else {
+                //if any rolls are not the same
+                //break the loop and return false
+                $super = false;
+                return $super;
+            }
+        }
+        return $super;
+    }
+    
+    function rerollCheck($stat1,$stat2,$sky){
+        if ($stat1 > $stat2 && $sky1 > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 ?>
